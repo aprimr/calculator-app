01b8698c3083d2ecda170fdec3d144e659195c05
aprimr <aprimregmi24@gmail.com>
2026-01-07 19:54:43 +0545

    fix: some fixes in more screen

--- lib/screens/more.dart
+++ lib/screens/more.dart
@@ -46,55 +46,53 @@ class More extends StatelessWidget {
       appBar: AppBar(title: Text("More")),
       body: Container(
         margin: EdgeInsets.only(left: 20, right: 20, top: 60),
-        child: Expanded(
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              mainAxisSpacing: 38,
-              crossAxisSpacing: 20,
-            ),
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: 9,
-            itemBuilder: (context, index) {
-              final item = items[index];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, itemRoutes[item]!);
-                },
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF111111),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Center(
-                          child: HugeIcon(
-                            icon: icons[index],
-                            size: 34,
-                            color: Colors.white,
-                          ),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 38,
+            crossAxisSpacing: 20,
+          ),
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: 9,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, itemRoutes[item]!);
+              },
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF111111),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: HugeIcon(
+                          icon: icons[index],
+                          size: 34,
+                          color: Colors.white,
                         ),
-                        SizedBox(height: 14),
-                        Text(
-                          items[index],
-                          style: TextStyle(
-                            color: const Color(0xFFA5A5A5),
-                            fontFamily: GoogleFonts.poppins().fontFamily,
-                            fontWeight: FontWeight.w600,
-                          ),
+                      ),
+                      SizedBox(height: 14),
+                      Text(
+                        items[index],
+                        style: TextStyle(
+                          color: const Color(0xFFA5A5A5),
+                          fontFamily: GoogleFonts.poppins().fontFamily,
+                          fontWeight: FontWeight.w600,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              );
-            },
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
